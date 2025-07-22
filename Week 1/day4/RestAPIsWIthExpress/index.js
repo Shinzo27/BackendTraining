1a95cba14ec390920783b894f90e87c89e63393a
Shinzo27 <20bmiit031@gmail.com>
2025-07-22 17:05:20 +0530

    Day-1,2

--- Week 1/day4/RestAPIsWIthExpress/index.js
+++ Week 1/day4/RestAPIsWIthExpress/index.js
@@ -1,86 +1,123 @@
-import express from 'express'
+import express from "express";
+import fs from "fs";
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = 8000;
 
-app.use(express.json())
+app.use(express.json());
 
-const products = [
-    {
-        id: 1,
-        name: "Cashew",
-        price: 550
-    },
-    {
-        id: 2,
-        name: "Almonds",
-        price: 700
-    },
-    {
-        id: 3,
-        name: "Walnuts",
-        price: 300
-    }
-]
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Server is healthy.",
+  });
+});
 
-app.get('/', (req, res) => {
+app.get("/getProducts", async (req, res) => {
+  let products;
+  await fs.readFile("./products.json", "utf8", (err, data) => {
+    if (err) return err;
+    console.log(data);
+    products = JSON.parse(data);
     return res.json({
-        message: "Server is healthy."
-    })
-})
+      status: 200,
+      products: products,
+    });
+  });
+});
 
-app.get('/getProducts', (req,res) => {
-    return res.json({
-        status: 200,
-        products: products
-    })
-})
+app.post("/addProduct", async (req, res) => {
+  const body = req.body;
+  let products;
+  await fs.readFile("./products.json", "utf8", async (err, data) => {
+    if (err) return err;
 
-app.post('/addProduct', (req, res) => {
-    const body = req.body
+    products = JSON.parse(data);
 
-    products.push(body)
+    products.push(body);
 
-    return res.json({
-        status: 200,
-        message: "Product Added Successfully!"
-    })
-})
+    await fs.writeFile("./products.json", JSON.stringify(products), (err) => {
+      if (err)
+        return res.status(500).json({
+          message: "Something went wrong",
+        });
+    });
+  });
 
-app.put('/editProduct/:id', (req, res) => {
-    const { id } = req.params
-    const body = req.body
+  return res.json({
+    status: 200,
+    message: "Product Added Successfully!",
+  });
+});
 
-    const index = products.findIndex((t) => t.id === Number(id))
-    if(index === -1) return res.json({
-        status: 500, 
-        message: "Product not found!"
-    })
-    products[index] = { ...products[index], ...body}
-
-    return res.json({
-        status: 200,
-        message: "Product Edited Successfully!"
-    })
-})
-
-app.delete('/deleteProduct/:id', (req, res) => {
-    const { id } = req.params
-
-    const index = products.findIndex((t) => t.id === Number(id))
-    if(index === -1) return res.json({
+app.put("/editProduct/:id", async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  let products;
+  await fs.readFile("./products.json", "utf8", async (err, data) => {
+    if (err)
+      return res.status(500).json({
+        message: "Something went wrong",
+      });
+    products = JSON.parse(data);
+    console.log(products);
+    const index = products.findIndex((t) => t.id === Number(id));
+    console.log(index);
+    if (index === -1)
+      return res.json({
         status: 500,
-        message: "Product Not Found!"
-    })
+        message: "Product not found!",
+      });
+    products[index] = { ...products[index], ...body };
 
-    products.splice(index, 1)
-    
+    console.log(products);
+    await fs.writeFile("./products.json", JSON.stringify(products), (err) => {
+      if (err)
+        return res.status(500).json({
+          message: "Something went wrong",
+        });
+    });
     return res.json({
-        status: 200,
-        message: "Product Deleted Successfully!"
-    })
-})
+      status: 200,
+      message: "Product Edited Successfully!",
+    });
+  });
+});
+
+app.delete("/deleteProduct/:id", async (req, res) => {
+  const { id } = req.params;
+  let products;
+  
+  await fs.readFile("./products.json", "utf8", async(err, data) => {
+    if (err)
+      return res.status(500).json({
+        message: "Something went wrong",
+      });
+    products = JSON.parse(data);
+
+    const index = products.findIndex((t) => t.id === Number(id));
+
+    if (index === -1)
+      return res.json({
+        status: 500,
+        message: "Product Not Found!",
+      });
+
+    products.splice(index, 1);
+    
+    await fs.writeFile("./products.json", JSON.stringify(products), (err) => {
+      if (err)
+        return res.status(500).json({
+          message: "Something went wrong",
+        });
+    });
+
+    return res.json({
+      status: 200,
+      message: "Product Deleted Successfully!",
+    });
+  });
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on port: "+ PORT);
-})
+  console.log("Server is running on port: " + PORT);
+});
