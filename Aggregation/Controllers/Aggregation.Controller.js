e4bba9ec8fe1ef1f5e0615e346a8f5f461bb6b61
Shinzo27 <20bmiit031@gmail.com>
2025-07-26 12:00:42 +0530

    Practice of Aggregation

--- Aggregation/Controllers/Aggregation.Controller.js
+++ Aggregation/Controllers/Aggregation.Controller.js
@@ -55,81 +55,316 @@ export const getAverageStocks = async (req, res) => {
   });
 };
 
-export const TotalValueofInventory = async(req, res) => {
-    const products = await Product.aggregate([
-        {
-            $addFields: {
-                totalValue: { $add: ['$price', '$stock']}
-            }
+export const TotalValueofInventory = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        totalValue: { $add: ["$price", "$stock"] },
+      },
+    },
+    {
+      $group: {
+        _id: "_id",
+        sumTotalValue: { $sum: "$totalValue" },
+      },
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const getHighestStockProduct = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $sort: {
+        stock: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const groupCategoryWishProducts = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        totalProducts: { $sum: 1 },
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const getAveragePriceCategory = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        averagePrice: { $avg: "$price" },
+      },
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const getTotalStockofCategory = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        totalStock: { $sum: "$stock" },
+      },
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const getCategoryWithMostProduct = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $group: {
+        _id: "$category",
+        totalProducts: { $sum: 1 },
+      },
+    },
+    {
+      $sort: {
+        totalProducts: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const getTotalInventoryRevenue = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        totalValue: { $add: ["$price", "$stock"] },
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        totalRevenue: {
+          $sum: "$totalValue",
         },
-        {
-            $group: {
-                _id: '_id',
-                sumTotalValue: { $sum: '$totalValue'}
-            }
+      },
+    },
+    {
+      $sort: {
+        totalRevenue: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const getStocksBelow10 = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $match: {
+        stock: { $lt: 10 },
+      },
+    },
+  ]);
+  return res.json({
+    products,
+  });
+};
+
+export const addInventoryValue = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        inventoryValue: { $multiply: ["$price", "$stock"] },
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const getMostValuableProduct = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        inventoryValue: { $multiply: ["$price", "$stock"] },
+      },
+    },
+    {
+      $sort: {
+        inventoryValue: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const sortProductByInventoryValue = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        inventoryValue: { $multiply: ["$price", "$stock"] },
+      },
+    },
+    {
+      $sort: {
+        inventoryValue: -1,
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const getAverageInventoryValuePerCategory = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $addFields: {
+        inventoryValue: { $multiply: ["$price", "$stock"] },
+      },
+    },
+    {
+      $group: {
+        _id: "$category",
+        averageValue: { $avg: "$inventoryValue" },
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const findProductPriceBetween100to500 = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $match: {
+        $and: [{ price: { $gt: 100 } }, { price: { $lt: 500 } }],
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const findProductNameWithUltraKeyword = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $match: {
+        title: { $regex: /.*Ultra.*/ },
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const getProductsofGroceriesWith50Stock = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $match: {
+        $and: [
+          { category: { $eq: "groceries" } },
+          {
+            stock: { $gt: 50 },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res.json({
+    products,
+  });
+};
+
+export const getProductwithMoreStockThanAvg = async (req, res) => {
+  const products = await Product.aggregate([
+    {
+      $setWindowFields: {
+        output: {
+          averageQuantity: {
+            $avg: "$stock",
+            window: {}
+          }
         }
-    ])
-    return res.json({
-        products
-    })
-}
+      }
+    },
+    {
+      $match: {
+        $expr: { $gt: ['$stock', '$averageQuantity']}
+      } 
+    },
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+        price: 1,
+        stock: 1,
+        averageQuantity: 1,
+      }
+    }
+  ]);
 
-export const getHighestStockProduct = async(req, res) => {
-    const products = await Product.aggregate([
-        {
-            $sort: {
-                'stock': -1
-            }
-        },
-        {
-            $limit: 1
-        }
-    ])
+  return res.json({
+    products,
+  });
+};
 
-    return res.json({
-        products
-    })
-}
-
-export const groupCategoryWishProducts = async(req, res) => {
-    const products = await Product.aggregate([
-        {
-            $group: {
-                _id: '$category',
-                totalProducts: { $sum: 1 }
-            }
-        }
-    ])
-
-    return res.json({
-        products
-    })
-}
-
-export const getAveragePriceCategory = async(req, res) => {
-    const products = await Product.aggregate([
-        {
-            $group: {
-                _id: '$category',
-                averagePrice: { $avg: '$price'}
-            }
-        }
-    ])
-    res.json({
-        products
-    })
-}
-
-export const getTotalStockofCategory = async(req, res) => {
-    const products = await Product.aggregate([
-        {
-            $group: {
-                _id: '$category',
-                totalStock: { $sum: '$stock' }
-            }
-        }
-    ])
-    res.json({
-        products
-    })
+export const convertToUppercase = async(req, res) => {
+  const products = await Product.aggregate([
+    {
+      $project: {
+        title: { $toUpper: "$title" },
+        stock: 1,
+        price: 1,
+        category: 1
+      }
+    }
+  ])
+  return res.json({
+    products
+  })
 }
--- Aggregation/Routes/Aggregation.route.js
+++ Aggregation/Routes/Aggregation.route.js
@@ -1,16 +1,56 @@
 import { Router } from "express";
-import { getAveragePriceCategory, getAverageStocks, getHighestStockProduct, getProducts, getTotalProducts, getTotalStockofCategory, getTotalStocks, groupCategoryWishProducts, TotalValueofInventory } from "../Controllers/Aggregation.Controller.js";
+import {
+  addInventoryValue,
+  convertToUppercase,
+  findProductNameWithUltraKeyword,
+  findProductPriceBetween100to500,
+  getAverageInventoryValuePerCategory,
+  getAveragePriceCategory,
+  getAverageStocks,
+  getCategoryWithMostProduct,
+  getHighestStockProduct,
+  getMostValuableProduct,
+  getProducts,
+  getProductsofGroceriesWith50Stock,
+  getProductwithMoreStockThanAvg,
+  getStocksBelow10,
+  getTotalInventoryRevenue,
+  getTotalProducts,
+  getTotalStockofCategory,
+  getTotalStocks,
+  groupCategoryWishProducts,
+  sortProductByInventoryValue,
+  TotalValueofInventory,
+} from "../Controllers/Aggregation.Controller.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/getAllProducts', getProducts)
-router.get('/getTotalProducts', getTotalProducts)
-router.get('/getTotalStocks', getTotalStocks)
-router.get('/getAverageStocks', getAverageStocks)
-router.get('/totalValueofInventory', TotalValueofInventory)
-router.get('/getHighestStockProduct', getHighestStockProduct)
-router.get('/groupCategoryWishProducts', groupCategoryWishProducts)
-router.get('/getAveragePriceCategory', getAveragePriceCategory)
-router.get('/getTotalStockofCategory', getTotalStockofCategory)
+router.get("/getAllProducts", getProducts);
+router.get("/getTotalProducts", getTotalProducts);
+router.get("/getTotalStocks", getTotalStocks);
+router.get("/getAverageStocks", getAverageStocks);
+router.get("/totalValueofInventory", TotalValueofInventory);
+router.get("/getHighestStockProduct", getHighestStockProduct);
+router.get("/groupCategoryWishProducts", groupCategoryWishProducts);
+router.get("/getAveragePriceCategory", getAveragePriceCategory);
+router.get("/getTotalStockofCategory", getTotalStockofCategory);
+router.get("/getCategoryWithMostProduct", getCategoryWithMostProduct);
+router.get("/getTotalInventoryRevenue", getTotalInventoryRevenue);
+router.get("/getStocksBelow10", getStocksBelow10);
+router.get("/addInventoryValue", addInventoryValue);
+router.get("/getMostValuableProduct", getMostValuableProduct);
+router.get("/sortProductByInventoryValue", sortProductByInventoryValue);
+router.get(
+  "/getAverageInventoryValuePerCategory",
+  getAverageInventoryValuePerCategory
+);
+router.get("/findProductPriceBetween100to500", findProductPriceBetween100to500);
+router.get("/findProductNameWithUltraKeyword", findProductNameWithUltraKeyword);
+router.get(
+  "/getProductsofGroceriesWith50Stock",
+  getProductsofGroceriesWith50Stock
+);
+router.get("/getProductwithMoreStockThanAvg", getProductwithMoreStockThanAvg);
+router.get("/convertToUppercase", convertToUppercase);
 
-export default router
+export default router;
